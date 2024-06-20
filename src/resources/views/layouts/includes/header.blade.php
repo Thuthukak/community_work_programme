c204243777af5bb51092b6860960133f61a2f73a
thuthukakhoza <thuthukakhoza@gmail.com>
2024-06-20 14:16:58 +0200

    project page touchups

--- src/resources/views/layouts/includes/header.blade.php
+++ src/resources/views/layouts/includes/header.blade.php
@@ -3,6 +3,7 @@
 {{ style(mix('css/core.css')) }}
 {{ style(mix('css/fontawesome.css')) }}
 {{ style('vendor/summernote/summernote-bs4.css') }}
-{{ Html::script(url('assets/js/plugins/select2.min.js')) }}
+<script src="{{ url('assets/js/plugins/select2.min.js') }}"></script>
+
 
 @stack('after-styles')
