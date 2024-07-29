beffbff04c6cdbda9c36345341f88eea828e5000
Itumeleng Mohlala <mohlala.itu.m@gmail.com>
2024-07-29 13:16:46 +0200

    all app

--- src/app/Http/Controllers/ProjectManagement/JobsController.php
+++ src/app/Http/Controllers/ProjectManagement/JobsController.php
@@ -43,7 +43,7 @@ class JobsController extends Controller
      *
      * @return \Illuminate\View\View
      */
-    public function index()
+    public function index($request)
     {
         $user = auth()->user();
         $User = new User();
