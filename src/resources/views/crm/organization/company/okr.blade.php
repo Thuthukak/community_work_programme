9be30ade9eb84b01d253bd06a5b9ad1ba49d82e5
thuthukakhoza <thuthukakhoza@gmail.com>
2024-08-05 15:01:54 +0200

    Objectives UI full screen

--- src/resources/views/crm/organization/company/okr.blade.php
+++ src/resources/views/crm/organization/company/okr.blade.php
@@ -58,10 +58,38 @@
 </style>
 @section('title','Objective')
 @section('contents')
-<div class="container">
+<div class="row align-items-center justify-content-between" style="margin: 60px;">
+    <h4 class="header-pill col-auto">Objectives and Key Results</h4>
+    <div class="col-auto">
+        <!-- Button trigger modal -->
+        <div class="" style="top: 100px; right: 50px;">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#objective">
+                Add Objective
+            </button>
+        </div>
+        @can('storeObjective', $company)
+        <a href="#" data-toggle="modal" data-target="#objective" class="newObjective"></a>
+        <div class="modal {{ count($errors) == 0 ? 'fade' : '' }}" id="objective" tabindex="-1" role="dialog">
+            <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">
+                            <span>&times;</span>
+                        </button>
+                    </div>
+                    @include('crm.okrs.create', ['route'=>route('company.objective.store', $company->id)])
+                </div>
+            </div>
+        </div>
+        @endcan
+    </div>
+</div>
+
+<div class="container-fluid" style="margin-right:60px;">
     @include('crm.organization.company.show')
     <div class="tab-pane fade show pl-sm-4 pr-sm-4">
-        <div class="filters">
+        <div class="row m-3 pt-4 justify-content-start" style="margin 20px;">
+            <div class="filters">
         <div class="filter-item">
             <button class="filter-btn" style="border-radius: 25px; padding:12px" id="datefilter">Date Range</button>
             <div id="dateDropdown" class="dropdown-content">
@@ -117,7 +145,7 @@
     </div>
         @if ($company->okrs)
             @foreach($company->okrs as $okr)
-                @include('crm.okrs.okr', ['okr' => $okr, 'owner'=>$company])
+                @include('crm.okrs.okr', ['okr' => $okr, 'owner' => $company])
             @endforeach
         @else
             <div id="dragCard" class="row justify-content-md-center u-mt-16">
@@ -128,36 +156,13 @@
             </div>
         @endif
     </div>
-  <!-- Button trigger modal -->
-  <div class="position-fixed" style="top: 100px; right: 50px;">
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#objective">
-                <img src="{{ asset('img/icon/add/lightgreen.svg') }}" alt="Add Objective">
-            </button>
-        </div>
-        @can('storeObjective', $company)
-        <a href="#" data-toggle="modal" data-target="#objective" class="newObjective"></a>
-        <div class="modal {{ count($errors) == 0 ? 'fade' : '' }}" id="objective" tabindex="-1" role="dialog">
-            <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">
-                            <span>&times;</span>
-                        </button>
-                    </div>
-                    @include('crm.okrs.create', ['route'=>route('company.objective.store', $company->id)])
-                </div>
-            </div>
-        </div>
-        </div>
-    @endcan
 </div>
+
 @endsection
 
 @section('script')
 <!-- Include Flatpickr JS from cdnjs -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.6/flatpickr.min.js"></script>
-
-
 
 <!-- Your script to initialize Flatpickr -->
 <script>
@@ -166,277 +171,6 @@
             dateFormat: "Y-m-d",
             disableMobile: true // optional: to force the desktop version on mobile devices
         });
-
-
-        var AddActionUrl = "{{ route('actions.create', ['objective' => ':id']) }}";
-
-        document.querySelectorAll('.add-action-btn').forEach(function (button) {
-        button.addEventListener('click', function () {
-        var objectiveId = this.getAttribute('data-id');
-        var url = AddActionUrl.replace(':id', objectiveId);
-
-        console.log(objectiveId);
-        
-        fetch( url , {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => {
-            console.log(response);
-            if (!response.ok) {
-
-                throw new Error(url +'Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-
-            console.log('Fetched Data:', data); // Debugging line
-
-            document.querySelector('#createActionForm').reset();
-            document.querySelector('#priority').innerHTML = '';
-            document.querySelector('#keyresult').innerHTML = '';
-
-            // Populate priorities
-            let prioritySelect = document.querySelector('#priority');
-            prioritySelect.innerHTML = '<option value="">Select Priority</option>';
-            data.priorities.forEach(priority => {
-                prioritySelect.innerHTML += `<option value="${priority.id}">${priority.priority}</option>`;
-            });
-
-            // Populate keyresults
-            if (data.keyresults) {
-                data.keyresults.forEach(function(keyresult) {
-                    document.querySelector('#keyresult').innerHTML += `<option value="${keyresult.id}">${keyresult.title}</option>`;
-                });
-            }
-
-            // Populate action_on with static options
-            // const actionOnOptions = ['Onboarding', 'Project', 'Proposal'];
-            // actionOnOptions.forEach(function(option) {
-            //     document.querySelector('#model_type').innerHTML += `<option value="${option}">${option.charAt(0).toUpperCase() + option.slice(1)}</option>`;
-            // });
-
-            $('#createActionModal').modal('show');
-        })
-        .catch(error => {
-                console.error('Error fetching data:', error);
-            });
-        });
     });
-
-
-
-
-        document.querySelector('#model_type').addEventListener('change', function() {
-            var actionOn = this.value;
-            // Make sure actionOn is not empty
-            if (actionOn) {
-                fetchModels(actionOn);
-            }
-        });
-
-        function fetchModels(actionOn) {
-            fetch(`{{ url('actions/models') }}/${actionOn}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            .then(response => {
-                console.log('Response Status:', response.status); // Debugging line
-                return response.json();
-            })
-            .then(data => {
-                console.log('Fetched Data:', data); // Debugging line
-                let modelSelect = document.querySelector('#model_id');
-
-                if (modelSelect) {
-                    modelSelect.innerHTML = '<option value="">Select Target</option>';
-                    data.models.forEach(model => {
-                        modelSelect.innerHTML += `<option value="${model.id}">${model.name}</option>`;
-                    });
-
-                    let lastModel = data.models.filter(model => typeof model === 'object').pop();
-                if (lastModel) {
-                    document.querySelector('#full_model_type').value = lastModel.model;
-                    console.log(document.querySelector('#full_model_type').value);
-                } else {
-                    console.error('No valid model object found');
-                }
-                } else {
-                    console.error('Element with id "model_id" not found');
-                }
-
-
-            })
-            .catch(error => {
-                console.error('Error fetching models:', error);
-            });
-        }
-
-
-
-        document.getElementById("Organizationlist").addEventListener("click", function() {
-            event.stopPropagation();
-            var dropdown = document.getElementById('organizationDropdown');
-            if (dropdown.style.display === "block") {
-                dropdown.style.display = "none";
-            } else {
-                closeAllDropdowns();
-                dropdown.style.display = "block";
-
-                var url = `{{ route('organization.get') }}`;
-
-                fetch(url, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-Requested-With': 'XMLHttpRequest'
-                    }
-                })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    console.log('Fetched data:', data);
-
-                    const organizationListContainer = document.getElementById('organizationListContainer');
-                    organizationListContainer.innerHTML = '';                  
-                    const hr = document.createElement('hr');
-
-
-                    for (const [id, name] of Object.entries(data)) {
-                        const listItem = document.createElement('li');
-
-                        
-                        const checkbox = document.createElement('input');
-                        checkbox.type = 'checkbox';
-                        checkbox.id = `organization-${id}`;
-                        checkbox.value = id;
-                        checkbox.classList.add('organization-checkbox');
-
-                        const label = document.createElement('label');
-                        label.htmlFor = `organization-${id}`;
-                        label.textContent = name;
-
-                        listItem.appendChild(checkbox);
-                        listItem.appendChild(label);
-                        listItem.classList.add('organization-item');
-                        
-                        organizationListContainer.appendChild(listItem);
-                    }
-                    organizationListContainer.appendChild(hr);
-
-                })
-                .catch(error => {
-                    console.error('Error fetching organization data:', error);
-                });
-            }
-        });
-
-        document.getElementById("clearOrganizations").addEventListener("click", function() {
-            const checkboxes = document.querySelectorAll('.organization-checkbox');
-            checkboxes.forEach(checkbox => {
-                checkbox.checked = false;
-            });
-        });
-
-        document.getElementById("applyOrganizations").addEventListener("click", function() {
-            const selectedOrganizations = [];
-            const checkboxes = document.querySelectorAll('.organization-checkbox:checked');
-            checkboxes.forEach(checkbox => {
-                selectedOrganizations.push({
-                    id: checkbox.value,
-                    name: checkbox.nextElementSibling.textContent
-                });
-            });
-            console.log('Selected Organizations:', selectedOrganizations);
-            document.getElementById('organizationDropdown').style.display = 'none';
-        });
-
-
-        function selectOrganization(id, name) {
-            console.log('Selected Organization:', id, name);
-            document.getElementById('organizationDropdown').style.display = 'none';
-        }
-
-        document.getElementById("it-has").addEventListener("click", toggleIsWith);
-
-        function toggleIsWith(event) {
-            event.stopPropagation();
-            var dropdown = document.getElementById('classDropdown');
-            if (dropdown.style.display === "block") {
-                dropdown.style.display = "none";
-            } else {
-                closeAllDropdowns();
-                dropdown.style.display = "block";
-            }
-        }
-
-        document.getElementById("clearClauses").addEventListener("click", function() {
-            const checkboxes = document.querySelectorAll('#clausesForm input[type="checkbox"]');
-            checkboxes.forEach(checkbox => {
-                checkbox.checked = false;
-            });
-        });
-
-        document.getElementById("applyClauses").addEventListener("click", function() {
-            const selectedClauses = [];
-            const checkboxes = document.querySelectorAll('#clausesForm input[type="checkbox"]:checked');
-            checkboxes.forEach(checkbox => {
-                selectedClauses.push(checkbox.name);
-            });
-            console.log('Selected Clauses:', selectedClauses);
-            document.getElementById('classDropdown').style.display = 'none';
-        });
-
-        document.getElementById("Projectvalue").addEventListener("click", toggleProjectValue);
-
-        function toggleProjectValue(event) {
-            event.stopPropagation();
-            var dropdown = document.getElementById('valueDropdown');
-            if (dropdown.style.display === "block") {
-                dropdown.style.display = "none";
-            } else {
-                closeAllDropdowns();
-                dropdown.style.display = "block";
-            }
-        }
-
-        document.getElementById("clearValues").addEventListener("click", function() {
-            document.getElementById("minValue").value = '';
-            document.getElementById("maxValue").value = '';
-        });
-
-        document.getElementById("applyValues").addEventListener("click", function() {
-            var minValue = document.getElementById("minValue").value;
-            var maxValue = document.getElementById("maxValue").value;
-            console.log("Min Value:", minValue, "Max Value:", maxValue);
-            // Add your code to handle the selected values
-            document.getElementById('valueDropdown').style.display = 'none';
-        });
-
-
-        function closeAllDropdowns() {
-            var dropdowns = document.getElementsByClassName("dropdown-content");
-            for (var i = 0; i < dropdowns.length; i++) {
-                dropdowns[i].style.display = "none";
-            }
-        }
-
-        window.onclick = function(event) {
-            if (!event.target.matches('.filter-btn') && !event.target.closest('.dropdown-content')) {
-                closeAllDropdowns();
-            }
-        }
-
-    });
-        });
 </script>
 @endsection
