5220d0b0edae1f2ee68a58804c4d499131498890
thuthukakhoza <thuthukakhoza@gmail.com>
2024-09-06 09:06:55 +0200

    slight changes

---
+++ src/resources/lang/en/objectives.php
@@ -0,0 +1,28 @@
+<?php
+
+    return [
+
+      'objective' => 'Objective',
+      'edit_objective' => 'Edit Objective',
+      'delete_objetive' => 'After deleting Objective,',
+      'delete_message' => 'You will lose KR and ACTION under Objective section.',
+      'delete_message2' => 'Do you confirm that you want to delete Objective?',
+      'key_results' => 'Key Results',
+      'edit_key_result' => 'Edit Key Result',
+      'key_result' => 'Key Result',
+      'reaching_rate' => 'Reaching Rate',
+      'start_value' => 'Start Value',
+      'target_value' => 'Target Value',
+      'current_value' => 'Current Value',
+      'weights' => 'Weights',
+      'confidence' => 'Confidence',
+      'modify' => 'Modify',
+      'after_delete' => 'After deleting Key Result, ',
+      'will_lose' => 'You will lose the action under Key Result section.',
+      'confirm' => 'Do you confirm that you want to delete Key Result?',
+      'actions' => 'Actions',
+      'message' => 'Message',
+      'historical_data' => 'Historical Data',
+
+
+    ];
